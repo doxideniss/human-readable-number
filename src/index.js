b253212d093a546852c3a88c34b5763217562660
Deniss <doxideniss@gmail.com>
2020-03-07 00:17:18 +0200

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const readableNumber = {
+        '0': ['zero'],
+        '1': ['one'],
+        '2': ['two', 'twenty'],
+        '3': ['three', 'thirty'],
+        '4': ['four', 'forty'],
+        '5': ['five', 'fifty'],
+        '6': ['six', 'sixty'],
+        '7': ['seven', 'seventy'],
+        '8': ['eight', 'eighty'],
+        '9': ['nine', 'ninety'],
+        '10': ['ten'],
+        '11': ['eleven'],
+        '12': ['twelve'],
+        '13': ['thirteen'],
+        '14': ['fourteen'],
+        '15': ['fifteen'],
+        '16': ['sixteen'],
+        '17': ['seventeen'],
+        '18': ['eighteen'],
+        '19': ['nineteen']
+    }
+
+    let strNumber = String(number)
+    let str = ''
+
+    if (number < 1000 && number >= 100) {
+        str = readableNumber[strNumber.slice(0,1)][0] + ' hundred '
+        if (strNumber.slice(1,2) !== '0') {
+            str += (readableNumber[strNumber.slice(1)] ? readableNumber[strNumber.slice(1)][0] : (readableNumber[strNumber.slice(1,2)][1] + (
+                readableNumber[strNumber.slice(2)][0] === 'zero' ? '' : ' ' + readableNumber[strNumber.slice(2)][0]
+            )))
+        } else if (strNumber.slice(2) !== '0') {
+            str += readableNumber[strNumber.slice(2,3)][0]
+        }
+    } else if (number < 100 && number > 19) {
+        str += (readableNumber[strNumber] ? readableNumber[strNumber][0] : (readableNumber[strNumber.slice(0,1)][1] + (
+            readableNumber[strNumber.slice(1)][0] === 'zero' ? '' : ' ' + readableNumber[strNumber.slice(1)][0]
+        )))
+    } else {
+        str += readableNumber[strNumber][0]
+    }
+
+    return str.trim()
+
 }
